3457b63abe0abe0e96d0e52897ff9949b823170f
sgavilanezp2-lab <sgavilanezp2@unemi.edu.ec>
2025-11-24 20:00:51 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,12 @@
-// Inicialización de Firebase
+// Inicialización Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBU1oaDdq6qD4fTiLN41SAeQg6Kp06gDXk",
-    authDomain: "simulador-tics.firebaseapp.com",
-    projectId: "simulador-tics",
-    storageBucket: "simulador-tics.appspot.com",
-    messagingSenderId: "501091859008",
-    appId: "1:501091859008:web:80e4596d2adcb5adbf7da5",
-    measurementId: "G-5LFLE4MBPH"
+  apiKey: "AIzaSyBU1oaDdq6qD4fTiLN41SAeQg6Kp06gDXk",
+  authDomain: "simulador-tics.firebaseapp.com",
+  projectId: "simulador-tics",
+  storageBucket: "simulador-tics.appspot.com",
+  messagingSenderId: "501091859008",
+  appId: "1:501091859008:web:80e4596d2adcb5adbf7da5",
+  measurementId: "G-5LFLE4MBPH"
 };
 
 firebase.initializeApp(firebaseConfig);
